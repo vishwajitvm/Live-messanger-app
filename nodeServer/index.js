a6a62bc8818a3086faafd5d8649a8933d5b28f08
vishwajitvm <vishwajitmall50@gmail.com>
2023-05-07 21:26:29 +0530

    fix corus issue and also add client.js

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -6,6 +6,7 @@ const user = {} ;
 io.on('connection' , Socket => {
     //NEW USER JOIN SOCKET
     Socket.on('new-user-joined' , name => {
+        console.log("New user" , name)
         user[Socket.id] = name ;
         Socket.broadcast.emit('user-joined' , name) ; //this will emit a message to all user except the user who joied the chat
     })
