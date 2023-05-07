e6def7749e6885929501afcb8d474437ef14c9b2
vishwajitvm <vishwajitmall50@gmail.com>
2023-05-07 22:25:57 +0530

    user join chat and user chat system completed

--- js/client.js
+++ js/client.js
@@ -4,5 +4,33 @@ const form = document.getElementById('send-container') ;
 const messageInput = document.getElementById('messageInp') ;
 const messageContainer = document.querySelector(".container") ;
 
+const append = (message , position) => {
+    const messageElement = document.createElement('div') ;
+    messageElement.innerText = message ;
+    messageElement.classList.add('message')
+    messageElement.classList.add(position)
+    messageContainer.append(messageElement) ;
+}
+
+//My message send
+form.addEventListener('submit' , (e) => {
+    e.preventDefault() ;
+    const message = messageInput.value ;
+    append(`You: ${message}` , 'right') ;
+    socket.emit('send' , message) ;
+    messageInput.value = '' ;
+
+})
+
 const name = prompt("Enter Your Name to Join Chat!!") ;
 socket.emit('new-user-joined' , name) ;
+
+//user joined
+socket.on('user-joined' , name => {
+    append(`${name} Joined the chat` , 'right')
+})
+
+//user Recived
+socket.on('receive' , data => {
+    append(`${data.name}: ${data.message}` , 'left')
+})
